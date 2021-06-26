9a73331c4d5c429b384a5b1deb83c264d46e7f61
lee_moo <2933251549@qq.com>
2021-06-26 16:08:27 +0800

    second

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+const First = () => <p>Yemianyideneirong</p>
+const App = () => (
+  <Router>
+    <div>
+      <h1>react路由基础</h1>
+      <Link to="/first">yemianyi</Link>
+      <Route path="/first" component={First}></Route>
+    </div>
+  </Router>
+)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'))
