bf919b283b61346c5adbcf622dd82bee75fbf10c
lee_moo <2933251549@qq.com>
2021-07-03 15:20:53 +0800

    发布界面1.1

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -2,28 +2,32 @@ import React from 'react'
 import './index.scss'
 import NavHeader from '../../components/NavHeader'
 import { ImagePicker } from 'antd-mobile';
+import empty from '../../assets/img/empty_pic.png'
 
-// const data = [{
-//   url: 'https://zos.alipayobjects.com/rmsportal/PZUUCKTRIHWiZSY.jpeg',
-//   id: '2121',
-// }, {
-//   url: 'https://zos.alipayobjects.com/rmsportal/hqQWgTXdrlmVVYi.jpeg',
-//   id: '2122',
-// }];
+const data = [{ url: empty }];
 
 
 export default class btn extends React.Component {
-  // state = {
-  //   files: data,
-  // }
+  //图片选择器
+  state = {
+    files: data,
+
+  }
   onChange = (files, type, index) => {
-    console.log(files, type, index);
+    // console.log(files, type, index);
+    // const Islen = files.length;
+    // console.log(Islen)
+    // if (Islen % 2 === 0) {
+    //   this.setState({
+    //     files,
+    //   })
+    // }
     this.setState({
       files,
     });
   }
   render() {
-    // const { files } = this.state;
+    const { files } = this.state;
     return (
       <div>
         <NavHeader>发布</NavHeader>
@@ -34,12 +38,18 @@ export default class btn extends React.Component {
           resize: 'none'
         }}></textarea>
         <ImagePicker
-          // files={files}
+          style={{
+            width: '100%'
+          }}
+          files={files}
           onChange={this.onChange}
           onImageClick={(index, fs) => console.log(index, fs)}
-          // selectable={files.length < 5}
+          selectable={files.length < 5}
           accept="image/gif,image/jpeg,image/jpg,image/png"
+          length='3'
         />
+        <div className='footer'>123
+        </div>
       </div>
     )
   }
