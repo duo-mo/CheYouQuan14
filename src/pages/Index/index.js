fcff4d7334c783ee2fa1bab419dfca86a6c11c52
lee_moo <2933251549@qq.com>
2021-07-06 22:22:11 +0800

    开始首页布局

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,9 +1,14 @@
 
 import React from 'react'
+import './index.scss'
 import NavHeader from '../../components/NavHeader'
 import { Popover, NavBar } from 'antd-mobile';
 import tuwen from '../../assets/img/ugc_icon_发图文.png'
 import tiwen from '../../assets/img/03-发布.png'
+import chequan from '../../assets/img/2-车友圈.png'
+import picShow from '../../assets/img/defautle.png'
+import profile from '../../assets/img/Mask.png'
+import more from '../../assets/img/更多.png'
 const Item = Popover.Item;
 const myImg = src => <img src={src} className="am-icon am-icon-xs" alt="" />;
 
@@ -30,6 +35,7 @@ export default class Index extends React.Component {
   render() {
     return (<div>
       <NavHeader>
+        {/* 发布按钮 */}
         <NavBar
           mode="light"
           rightContent={
@@ -62,7 +68,9 @@ export default class Index extends React.Component {
                 color: '#333333',
                 borderRadius: '50px',
                 lineHeight: '50px',
-                boxShadow: '0 2px 8px 0'
+                boxShadow: '0 2px 8px 0',
+                fontFamily: 'PingFang - SC - Medium',
+                fontSize: '16px'
               }}
               >
                 {/* <Icon type="ellipsis" /> */}
@@ -74,6 +82,30 @@ export default class Index extends React.Component {
         </NavBar>
         车友圈
       </NavHeader>
-    </div>);
+      <nav className='nav'>
+        <span><img className='cqpic' src={chequan} alt='chequan'></img></span>
+        <span>热门车友圈</span>
+      </nav>
+      <nav className='Cciecle'>
+        <div className='card'>
+          <div className='title'>奥迪A4L奥迪A4L车友圈</div>
+          <div className='picShow'>
+            <img className='pics' src={picShow} alt='picShow'></img>
+            <img className='pics' src={picShow} alt='picShow'></img>
+            <img className='pics' src={picShow} alt='picShow'></img>
+          </div>
+          <div className='describe'>
+            <img src={profile} alt='picShow'></img>
+            <img src={profile} alt='picShow'></img>
+            <img src={profile} alt='picShow'></img>
+            <img src={more} alt='picShow'></img>
+            <div className='details'>30402位活跃车友</div>
+            <div className='button'>
+              <div className='join'>加入</div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div >);
   }
 }
